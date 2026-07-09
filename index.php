ec48f4a80a9523e6c98be0241dcec750c0d25cc1
linuxawsmani <linuxaws002@gmail.com>
2026-07-09 13:01:09 +0530

    new change index.php

--- index.php
+++ index.php
@@ -18,16 +18,16 @@ try {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tech Admin CI/CD pipeline AZ Test AWS ECS Fargate Demo Page</title>
+    <title>Tech Admin CI/CD pipeline AZ first Test AWS ECS Fargate Demo Page</title>
 </head>
 <body>
 
-    <h1>Tech Admin CI/CD AZ Test Application Running Successfully</h1>
+    <h1>Tech Admin CI/CD AZ first Test Application Running Successfully</h1>
 
-    <h2>Tech Admin CI/CD AZ Test Database Connectivity Status</h2>
+    <h2>Tech Admin CI/CD AZ first Test Database Connectivity Status</h2>
 
     <p style="color:green;">
-        Tech Admin CI/CD AZ Test Application connected to Amazon RDS MySQL Successfully
+        Tech Admin CI/CD AZ first Test Application connected to Amazon RDS MySQL Successfully
     </p>
 
     <p>
