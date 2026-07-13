44ae3f8143692397cb3b84c98e121ad3d30bbdda
linuxawsmani <linuxaws002@gmail.com>
2026-07-13 19:41:03 +0530

    changes index.php

--- index.php
+++ index.php
@@ -18,16 +18,16 @@ try {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tech Admin AZ Test AWS ECS Fargate Demo Page</title>
+    <title>Tech Admin AZ Test first AWS ECS Fargate Demo Page</title>
 </head>
 <body>
 
-    <h1>Tech Admin AZ Test Application Running Successfully</h1>
+    <h1>Tech Admin AZ Test first Application Running Successfully</h1>
 
-    <h2>Tech Admin AZ Test Database Connectivity Status</h2>
+    <h2>Tech Admin AZ Test first Database Connectivity Status</h2>
 
     <p style="color:green;">
-        Tech Admin AZ Test Application connected to Amazon RDS MySQL Successfully
+        Tech Admin AZ Test first Application connected to Amazon RDS MySQL Successfully
     </p>
 
     <p>
